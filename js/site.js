78b53c08cb25c56796c85b1ff3a5ba44ba016da9
Alejandro Garcia <alex-04@live.com>
2022-07-19 19:45:17 -0700

    Complete applicaton
    Functioning "app" still need minor details

--- js/site.js
+++ js/site.js
@@ -1,6 +1,52 @@
 
-//Call Hello World
-function HelloWorld(){
-    alert("Hello World");
-    console.log("Hello World");
-};
+
+function getValues() {
+  let startValue = document.getElementById("startValue").value;
+  let endValue = document.getElementById("endValue").value;
+
+
+  startValue = parseInt(startValue);
+  endValue = parseInt(endValue);
+
+  if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+   let numbers = generateNumbers(startValue, endValue);
+   displayNumbers(numbers);
+
+  } else {
+    alert("You must enter integers")
+  }
+  
+}
+
+
+function generateNumbers(sValue, eValue) {
+  let numbers = [];
+
+  for (let index = sValue; index <= eValue; index++) {
+    numbers.push(index);
+  }
+  return numbers;
+}
+
+
+
+
+
+
+function displayNumbers(numbers) {
+
+  let templateRows = "";
+  for (let index = 0; index < numbers.length; index++) {
+    let className = "even";
+    let number = numbers[index];
+    if(number % 2==0){
+      className = "even";
+    }else{
+      className = "odd";
+    }
+    templateRows += `<tr><td class= "${className}" >${number}</td></tr>`;
+  }
+  document.getElementById("results").innerHTML = templateRows;
+
+}
